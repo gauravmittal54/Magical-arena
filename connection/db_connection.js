3c0c1c9a70753d202018c00a08a1440873f14c19
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2024-03-05 15:46:07 +0530

    Delete connection directory

--- connection/db_connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUrl = 'mongodb://0.0.0.0/magicalArenaGame';
-
-mongoose.connect(mongoUrl, {
-}).then(() => {
-    console.log("Connection is successful");
-}).catch((e) => {
-    console.log("No connection: ", e);
-})
